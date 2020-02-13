909b58aafa303edb7eaf5e313fd6dd341c97716b
Sazzad Hossain Nirjhor <sazzad3029@gmail.com>
2020-02-13 17:29:42 +0600

    day-3

--- udemy_course/basic_react/react-complete-guide/src/App.js
+++ udemy_course/basic_react/react-complete-guide/src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    person: [
+      { name: "Sazzad", age: 25 },
+      { name: "Nirjhor", age: 25 },
+      { name: "Hossain", age: 25 },
+      { name: "Rusty", age: 25 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("Was Clicked !!");
+    //Don't do this: this.state.person[0].name = "Sazzad Hossain Nirjhor";
+    this.setState({
+      person: [
+        { name: "Nir", age: 25 },
+        { name: "Nirjhor", age: 25 },
+        { name: "Hossain", age: 25 },
+        { name: "Rusty", age: 25 },
+      ]
+    })
+  }
+  //  switchNameHandler(params) {
+  //   console.log("Was Clicked !!");
+  // }
+
   render() {
     return (
       <div className="App">
         <h1 >Hi, I'a React App</h1>
         <div>
           <p>This is really Working !!</p>
-          <Person name="Sazzad" />
-          <Person name="Hossain" />
-          <Person name="Nirjhor" />
-          <Person name="Rusty"> My Hobbies: Painting </Person>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <Person name={this.state.person[0].name} />
+          <Person name={this.state.person[1].name} />
+          <Person name={this.state.person[2].name} />
+          <Person name={this.state.person[3].name}> My Hobbies: Painting </Person>
         </div>
       </div>
     );
